c037b143153642c6c7801ab52cb65f755cdc2878
Minocu <145881949+Minocu@users.noreply.github.com>
2026-05-28 16:10:23 -0500

    Add files via upload

---
+++ app-monitoreo-mainv2/application/handlers.py
@@ -0,0 +1,48 @@
+from infrastructure.api_client import optimizar_cortes_api
+from infrastructure.pdf_generator import generar_pdf
+from infrastructure.database import guardar_pedido, consultar_historial, obtener_detalle_pedido
+import infrastructure.pdf_generator as test
+print(dir(test))
+
+
+def handle_optimizar(cortes):
+    if not cortes:
+        return {"error": "No hay cortes válidos"}
+
+    try:
+        data = optimizar_cortes_api(cortes)
+        return {"data": data}
+    except Exception as e:
+        return {"error": str(e)}
+    
+
+
+def handle_generar_pdf(info, data):
+    try:
+        return  generar_pdf(info, data)
+    except Exception as e:
+        return {"error": str(e)}
+
+
+def handle_guardar(cliente, producto, datos, resultado):
+    try:
+        guardar_pedido(cliente, producto, datos, resultado)
+        return {"ok": True}
+    except Exception as e:
+        return {"error": str(e)}
+
+
+def handle_obtenerHistorial():
+    try:
+        historial = consultar_historial()
+        return historial
+    except Exception as e:
+        return {"error": str(e)}
+    
+
+def handle_detallePedido(index):
+    try:
+        dt = obtener_detalle_pedido(index)
+        return dt
+    except Exception as e:
+        return {"error": str(e)}
---
+++ app-monitoreo-mainv2/infrastructure/database.py
@@ -0,0 +1,91 @@
+import sqlite3
+import pandas as pd
+import json
+from datetime import datetime
+
+DB_NAME = 'kingspan_pedidos.db'
+
+def iniciar_db():
+    conn = sqlite3.connect(DB_NAME)
+    cursor = conn.cursor()
+    
+    cursor.execute('''
+        CREATE TABLE IF NOT EXISTS pedidos (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            fecha_registro TEXT,
+            cliente TEXT,
+            producto TEXT,
+            datos_completos TEXT,
+            resultado_optimizacion TEXT
+        )
+    ''')
+    
+    conn.commit()
+    conn.close()
+
+
+def obtener_siguiente_orden():
+    conn = sqlite3.connect(DB_NAME)
+    cursor = conn.cursor()
+    
+    cursor.execute('SELECT MAX(id) FROM pedidos')
+    resultado = cursor.fetchone()[0]
+    
+    conn.close()
+    
+    return (resultado + 1) if resultado else 1
+
+
+def guardar_pedido(cliente, producto, datos_dict, resultado_list):
+    conn = sqlite3.connect(DB_NAME)
+    cursor = conn.cursor()
+    
+    cursor.execute('''
+        INSERT INTO pedidos 
+        (fecha_registro, cliente, producto, datos_completos, resultado_optimizacion)
+        VALUES (?, ?, ?, ?, ?)
+    ''', (
+        datetime.now().strftime("%Y-%m-%d %H:%M:%S"),
+        cliente,
+        producto,
+        json.dumps(datos_dict),
+        json.dumps(resultado_list)
+    ))
+    
+    conn.commit()
+    conn.close()
+
+
+def consultar_historial():
+    conn = sqlite3.connect(DB_NAME)
+    
+    df = pd.read_sql_query("""
+        SELECT 
+            id as 'N° Orden', 
+            fecha_registro as 'Fecha Registro', 
+            cliente as 'Cliente', 
+            producto as 'Producto' 
+        FROM pedidos 
+        ORDER BY id DESC
+    """, conn)
+    
+    conn.close()
+    return df
+
+
+def obtener_detalle_pedido(id_pedido):
+    conn = sqlite3.connect(DB_NAME)
+    cursor = conn.cursor()
+    
+    cursor.execute(
+        'SELECT datos_completos, resultado_optimizacion FROM pedidos WHERE id = ?', 
+        (id_pedido,)
+    )
+    
+    res = cursor.fetchone()
+    conn.close()
+    
+    if res:
+        return json.loads(res[0]), json.loads(res[1])
+    
+    return None, None
